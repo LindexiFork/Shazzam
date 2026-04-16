2627c99d18cb5a6eed6a9ec74a6d3dd3cea2fdff
lindexi <lindexi_gd@163.com>
2026-04-16 09:58:21 +0800

    Fix not found dll file.

---
+++ ShazzamUnitTests/Helpers/GlobalTestSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Windows;
+using NUnit.Framework;
+using Shazzam;
+
+//namespace ShazzamUnitTests.Helpers; // Keep no namespace, so that `[SetUpFixture]` can run before any test method.
+
+[SetUpFixture]
+public class GlobalTestSetup
+{
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
+    {
+        var manualResetEventSlim = new ManualResetEventSlim(initialState:false);
+        // Ensure the WPF application running
+        var wpfThread = new Thread(() =>
+        {
+            var application = new Application();
+            application.Startup += delegate
+            {
+                manualResetEventSlim.Set();
+            };
+            application.Run();
+        })
+        {
+            IsBackground = true,
+            Name = "WPF UI Thread"
+        };
+        wpfThread.SetApartmentState(ApartmentState.STA);
+        wpfThread.Start();
+
+        manualResetEventSlim.Wait();
+        manualResetEventSlim.Dispose();
+    }
+}
